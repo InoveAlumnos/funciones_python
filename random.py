af55838c00a7240779db80db73e033434095ba76
Rodolfo <rodo2005@gmail.com>
2020-06-09 23:50:57 -0300

    Random funciona
    Lo siguiente es incorporarlo al archivo de ejercicios.

--- random.py
+++ random.py
@@ -1,27 +1,37 @@
 import random
 azar = []
 print('Bien venido')
-
+inicio = 0
+fin = 0
+cantidad = 0
+azar = []
 def lista_aleatorea(inicio, fin, cantidad):
-    if inicio <= cantidad and cantidad >= fin:
-        azar= random.randrange(inicio, fin, cantidad)
-        print(azar)
-        return azar
+    i = 0
+    while i < cantidad:
+            azaroso= random.randrange(inicio, fin, cantidad)
+            azar.append(azaroso)
+            i += 1
     else:
-        print('Datos fuera de rango\n')
-        return
+        return azar
+
+        
 
     
 def obtener():
     inicio = int(input('Ingrese el parametro inicio\n'))
     fin = int(input('Ingrese el parametro fin\n'))
+    rango = fin - inicio
     cantidad = int(input('Ingrese el parametro cantidad\n'))
-    azar = lista_aleatorea(inicio, fin, cantidad)
-    print(azar)
+    if cantidad <= rango and cantidad > 0:
+        azar = lista_aleatorea(inicio, fin, cantidad)
+        print(azar)
+    else:
+        print('Datos fuera de rango\n')
+
     
     
 
 
 if __name__ == '__main__':
-    lista_aleatorea()
+    lista_aleatorea(inicio, fin, cantidad)
     obtener()
