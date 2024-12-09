6fdaebf95fda3f2555ba71f1322cd9b347a01713
inoveAlumnos <alumnos@inove.com.ar>
2024-12-08 21:11:18 -0300

    Ejercicios practica

---
+++ ejercicios_practica/ejercicio_5.py
@@ -0,0 +1,56 @@
+# CODE:33
+# Funciones [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 3.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios con funciones
+
+# Objetivo:
+# Reutilizar código escrito en desafios anteriores
+
+# --------------------------------
+# Alumno:
+# Aquí copiar la función "generar_invitados"
+# ya elaborada
+
+# --------------------------------
+
+# --------------------------------
+# Alumno:
+# Aquí copiar la función "ordenar"
+# ya elaborada
+
+# --------------------------------
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+
+    # Alumno: 
+    # Copiar las funciones "generar_invitados" y "ordenar"
+    # creadas en los ejercicios anteriores
+    # Deberá copiarlas fuera del bloque "__main__" como se detalla
+    # al comienzo del archivo
+    # --> El objetivo es generar una lista de invitados y ordenarla
+
+    # Luego de copiar las funciones, invocarla en este lugar:
+
+    # 1) Primero generar una lista de invitados con "generar_invitados"
+    #    Almacenar el retorno en "lista_invitados"
+
+    # lista_invitados = generar_invitados()
+
+    # 2) Luego ordenar la lista de invitados con "ordenar"
+    #    --> Pasar como parámetro la "lista_invitados"
+    #    --> Retornar la lista de invitados ordenada
+    #    --> Almacenar el retorno en "lista_invidatos_ordenada"
+
+    # lista_invidatos_ordenada = ordenar(lista_invitados)
+
+    # Imprimir en pantalla "lista_invidatos_ordenada":
+
+    print("terminamos")
